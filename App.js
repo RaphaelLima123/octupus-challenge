538032c79506642ed2fe34f4972b6acd4bce7b75
Raphael Lima <raphaellimag@gmail.com>
2021-04-14 13:45:24 -0300

    chore: add react-navigation

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is my App</Text>
+      <Text>This is my</Text>
       <StatusBar style={{ style: 'auto' }} />
     </View>
   );
