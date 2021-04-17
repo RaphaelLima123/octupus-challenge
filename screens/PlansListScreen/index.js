936e8bc71a4fc4657d90cf575412440f18040617
Raphael Lima <raphaellimag@gmail.com>
2021-04-16 21:17:26 -0300

    refactor: change the FlatList in PlansListScreen

--- screens/PlansListScreen/index.js
+++ screens/PlansListScreen/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { Container, TitleText } from './styles';
+import React, { useState, useEffect } from 'react';
+import { FlatList, View, Text } from 'react-native';
+import { Container, ItemView, TitleText } from './styles';
+import { getOctupusPlnas } from '../../services/octupusApi';
 
-const PlansListScreen = () => (
-  <Container>
-    <TitleText>This is the Plans Screen</TitleText>
-  </Container>
-);
+const PlansListScreen = ({ route, navigation: { navigate } }) => {
+  const [plans, setPlans] = useState([]);
+
+  useEffect(() => {
+    const getApiOctupus = async () => {
+      const location = route.params.coords;
+      const data = await getOctupusPlnas(location);
+      const array = data.list;
+      console.log('ESTE É O NOSSO ARRAY', array);
+      setPlans(array);
+    };
+
+    getApiOctupus();
+  }, []);
+
+  const ViewPlan = ({ name }) => (
+    <ItemView onPress={() => navigate('Mapa')}>
+      <TitleText>{name}</TitleText>
+    </ItemView>
+  );
+
+  return (
+    <Container>
+      <FlatList
+        data={plans}
+        renderItem={({ item }) => <ViewPlan name={item.name} />}
+        keyExtractor={(item, index) => index.toString()}
+        showsVerticalScrollIndicator={false}
+      />
+    </Container>
+  );
+};
 
 export default PlansListScreen;
--- screens/PlansListScreen/styles.js
+++ screens/PlansListScreen/styles.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  align-items: center;
+`;
 
-export const ItemView = styled.View``;
+export const ItemView = styled.TouchableOpacity`
+  align-items: center;
+  background-color: #2c6bed;
+  border-radius: 10px;
+  width: 300px;
+  height: 150px;
+  margin: 5%;
+  padding: 2%;
+`;
 
-export const TitleText = styled.Text``;
+export const TitleText = styled.Text`
+  font-family: Roboto;
+  font-weight: bold;
+  font-size: 20px;
+  color: #fff;
+`;
 
 export const ItemsText = styled.Text``;
