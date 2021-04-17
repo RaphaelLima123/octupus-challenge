794eab63658516ad3be93a5e391aff423f358a6d
Raphael Lima <raphaellimag@gmail.com>
2021-04-17 17:10:36 -0300

    feat: add a marker in the map

--- screens/MapScreen/index.js
+++ screens/MapScreen/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Marker, MarkerAnimated } from 'react-native-maps';
 import { Container, Map } from './styles';
 
 const MapScreen = ({ route }) => {
-  console.log('ESSA É MINHA ROUTE', route);
-  const { lat, lon } = route.params;
+  const [markers, setMarkers] = useState([]);
+  const { lat, lon } = route.params.coords;
+  const allPlans = route.params.plans;
+  const pins = [];
+
+  useEffect(() => {
+    allPlans.map((plan) =>
+      pins.push({
+        coordinate: {
+          latitude: plan.coords.lat,
+          longitude: plan.coords.lon,
+        },
+        title: plan.name,
+        description: plan.type,
+      }),
+    );
+    setMarkers(pins);
+  }, []);
+
   return (
     <Container>
       <Map
@@ -15,7 +33,19 @@ const MapScreen = ({ route }) => {
         }}
         showsUserLocation
         loadingEnabled
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index.toString()}
+            coordinate={{
+              latitude: marker.coordinate.latitude,
+              longitude: marker.coordinate.longitude,
+            }}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </Map>
     </Container>
   );
 };
